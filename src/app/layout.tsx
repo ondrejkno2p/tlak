7e635607285962dbce8ebe43f74178f37f8c56c3
Ondřej Knopp <knopp@karel.troja.mff.cuni.cz>
2023-10-05 03:45:24 +0200

    title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 export const metadata: Metadata = {
-  title: 'Test',
-  description: 'Test',
+  title: 'Tlak',
+  description: 'Tlak ve vaší lokaci',
 }
 
 export default function RootLayout({
