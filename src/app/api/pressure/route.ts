2c3ab20fa4edfa01dc26c94890491fff75889886
Ondřej Knopp <knopp@karel.troja.mff.cuni.cz>
2023-10-11 14:27:41 +0200

    date fix till eom

--- src/app/api/pressure/route.ts
+++ src/app/api/pressure/route.ts
@@ -6,15 +6,19 @@ export async function getData({q,}:{q?:string}) {
   const params={
     key:process.env.WEATHER_API_KEY as string,
     q:q?q:'Prague',
-    dt:now.getUTCFullYear()+'-'+String(now.getUTCMonth()+1).padStart(2,'0')+'-'+String(now.getUTCDay()).padStart(2,'0'),
-    end_dt:now.getUTCFullYear()+'-'+String(now.getUTCMonth()+1).padStart(2,'0')+'-'+String(now.getUTCDay()+1).padStart(2,'0')
+    dt:now.getUTCFullYear()+'-'+String(now.getUTCMonth()+1).padStart(2,'0')+'-'+String(now.getUTCDate()-1).padStart(2,'0'),
+    end_dt:now.getUTCFullYear()+'-'+String(now.getUTCMonth()+1).padStart(2,'0')+'-'+String(now.getUTCDate()).padStart(2,'0')
   }
+  // console.log(now.getUTCDay())
+  // console.log(params.dt, params.end_dt)
   const res=await fetch('http://api.weatherapi.com/v1/history.json?'+new URLSearchParams(params).toString())
   const body = await res.json()
+  // console.log(body.forecast.forecastday[0])
   const pressures:{pressure_KPa:number,time:number}[] = []
   body.forecast.forecastday.forEach((day:{hour:any[]}) => {
     day.hour.forEach((element:any) => {
       const dt =(new Date(element.time).getTime()-Date.now())/3600000;
+      // console.log(dt, new Date(element.time).getTime(), Date.now(), new Date(element.time).toLocaleDateString(),new Date(Date.now()).toLocaleDateString())
       if( dt>= -24 && dt <= 0){
         pressures.push({pressure_KPa:element.pressure_mb,time:dt})
       }
@@ -33,7 +37,7 @@ export async function getData({q,}:{q?:string}) {
 }
 
 export async function GET(request: Request,) {
-  const q = new URL(request.url).searchParams.get('q')
+  const q = new URL(request.url).searchParams.get('q');
   const pressures = await getData({q:q?q:undefined});
   return NextResponse.json(pressures,{headers:{'cache-control':'max-age=1800'}}) 
 } 
